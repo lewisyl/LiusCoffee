3fd7cfec34d4a2e112a3b8f7fa11515c62034e52
Lewis Liu <lewisliuusa@outlook.com>
2021-06-08 13:11:55 -0700

    fixed duplicated id issuue

--- app/src/main/java/dev/lewisliu/liuscoffee/Order.java
+++ app/src/main/java/dev/lewisliu/liuscoffee/Order.java
@@ -2,9 +2,8 @@ package dev.lewisliu.liuscoffee;
 
 public class Order {
     private String _item;
-    private double _price, _subtotal, _tax, _total;
+    private double _price;
     private int _quantity;
-    private double _taxRate = 0.11;
 
     public Order(String _item, double _price, int _quantity) {
         this._item = _item;
@@ -28,43 +27,11 @@ public class Order {
         this._price = _price;
     }
 
-    public double get_subtotal() {
-        return _subtotal;
-    }
-
-    public void set_subtotal(double _subtotal) {
-        this._subtotal = _subtotal;
-    }
-
-    public double get_tax() {
-        return _tax;
-    }
-
-    public void set_tax(double _tax) {
-        this._tax = _tax;
-    }
-
-    public double get_total() {
-        return _total;
-    }
-
-    public void set_total(double _total) {
-        this._total = _total;
-    }
-
     public int get_quantity() {
         return _quantity;
     }
 
     public void set_quantity(int _quantity) {
         this._quantity = _quantity;
-    }
-
-    public double get_taxRate() {
-        return _taxRate;
-    }
-
-    public void set_taxRate(double _taxRate) {
-        this._taxRate = _taxRate;
     }
 }
